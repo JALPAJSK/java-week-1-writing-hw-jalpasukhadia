8182faa414b3ece2cd43ab1bca9d97204af6e1d2
JALPAJSK <jalpajsk0421@gmail.com>
2023-10-19 15:01:15 +0100

    My homework week 1

---
+++ src/javaprogramme/Programme_3_PrintInitials.java
@@ -0,0 +1,15 @@
+package javaprogramme;
+
+public class Programme_3_PrintInitials {
+    public static void main(String[] args) {
+        System.out.println("*********  ******** ********* ********");
+        System.out.println("      **   **    **       **  **    **");
+        System.out.println("      **   **    **       **  **    **");
+        System.out.println("      **   **    **       **  **    **");
+        System.out.println("      **   ********       **  ********");
+        System.out.println("  **  **   **    **   **  **  **    **");
+        System.out.println("  **  **   **    **   **  **  **    **");
+        System.out.println("   ****    **    **    ****   **    **");
+        System.out.println("    **     **    **     **    **    **");
+    }
+}
